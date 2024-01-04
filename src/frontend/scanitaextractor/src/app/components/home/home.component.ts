1f92d93be37666d7589ac518bd29211a13245caf
Mattia Ricci <mattia.ricci.000@gmail.com>
2024-01-04 22:48:37 +0100

    updated scripts

--- src/frontend/scanitaextractor/src/app/components/home/home.component.ts
+++ src/frontend/scanitaextractor/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputFormComponent } from '../input-form/input-form.component';
+import { Meta } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-home',
@@ -14,4 +15,9 @@ import { InputFormComponent } from '../input-form/input-form.component';
 })
 export class HomeComponent {
 
+  constructor(private _meta: Meta) {
+    this._meta.addTag({ name: 'title', content: 'Scan Ita PDF Extractor' });
+    this._meta.addTag({ name: 'og:title', content: 'Scan Ita PDF Extractor' });
+    this._meta.addTag({ name: 'og:description', content: 'Transform you Scan Ita experience by creating high resolution PDFs for your favorite mangas!' });
+  }
 }
