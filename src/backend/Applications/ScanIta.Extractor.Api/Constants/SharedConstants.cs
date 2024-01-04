aa3c13bed9554965b5fedbcbf8dbbc4f76bd4cc0
Mattia Ricci <mattia.ricci.000@gmail.com>
2024-01-04 23:36:11 +0100

    added new endpoint

--- src/backend/Applications/ScanIta.Extractor.Api/Constants/SharedConstants.cs
+++ src/backend/Applications/ScanIta.Extractor.Api/Constants/SharedConstants.cs
@@ -3,5 +3,6 @@ namespace ScanIta.Crawler.Api.Constants;
 public static class SharedConstants
 {
     public static string ScanItaBaseUrl = "https://scanita.org";
+    public static string LinkPreviewBaseUrl = "https://api.linkpreview.net";
     public static string ScanItaClientName = "ScanIta";
 }
